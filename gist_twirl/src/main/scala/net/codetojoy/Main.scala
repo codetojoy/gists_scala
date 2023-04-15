233055e659ec178da5626ec6744095578df9fbe0
Michael Easter <codetojoy@gmail.com>
2023-04-15 10:28:19 -0300

    rename file

--- gist_twirl/src/main/scala/net/codetojoy/Main.scala
+++ gist_twirl/src/main/scala/net/codetojoy/Main.scala
@@ -1,7 +1,7 @@
 
 package net.codetojoy
 
-object Generator extends App {
+object Main extends App {
     // val content: Html = html.main(name = "Mozart")
     println(html.main(name = "Mozart")) 
     println("Ready.")
